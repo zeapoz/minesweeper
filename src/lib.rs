7e4c022d2b88ed1152a7bf612a9b63dd745a6e38
zeapoz <zeapo@pm.me>
2022-05-10 17:56:43 +0200

    Added uncover functionality

--- src/lib.rs
+++ src/lib.rs
@@ -9,9 +9,17 @@ use wasm_bindgen::prelude::*;
 static ALLOC: wee_alloc::WeeAlloc = wee_alloc::WeeAlloc::INIT;
 
 #[wasm_bindgen]
+#[repr(u8)]
 pub enum Tile {
     Empty,
     Mine,
+}
+
+#[wasm_bindgen]
+#[repr(u8)]
+pub enum TileState {
+    Covered,
+    Uncovered,
 }
 
 #[wasm_bindgen]
@@ -19,11 +27,13 @@ pub struct Board {
     width: u32,
     height: u32,
     tiles: Vec<Tile>,
+    uncovered: Vec<TileState>,
 }
 
 #[wasm_bindgen]
 impl Board {
     pub fn new(width: u32, height: u32) -> Board {
+        let uncovered = (0..width * height).map(|_| TileState::Covered).collect();
         let tiles = (0..width * height)
             .map(|i| {
                 if i % 2 == 0 && i % 3 != 0 {
@@ -38,10 +48,26 @@ impl Board {
             width,
             height,
             tiles,
+            uncovered,
         }
+    }
+
+    pub fn uncover_tile(&mut self, row: u32, col: u32) {
+        let i = self.get_index(row, col);
+        self.uncovered[i] = TileState::Uncovered;
     }
 
     pub fn tiles(&self) -> *const Tile {
         self.tiles.as_ptr()
+    }
+
+    pub fn uncovered(&self) -> *const TileState {
+        self.uncovered.as_ptr()
+    }
+}
+
+impl Board {
+    fn get_index(&self, row: u32, col: u32) -> usize {
+        (row * self.width + col) as usize
     }
 }
--- www/src/index.ts
+++ www/src/index.ts
@@ -1,5 +1,5 @@
 import { memory } from "minesweeper/minesweeper_bg.wasm";
-import { Board, Tile } from "minesweeper";
+import { Board, Tile, TileState } from "minesweeper";
 
 const SIZE = 20;
 const TILE_SIZE = 20;
@@ -10,6 +10,24 @@ const canvas = document.getElementById("minesweeper-canvas") as HTMLCanvasElemen
 canvas.width = SIZE * TILE_SIZE;
 canvas.height = SIZE * TILE_SIZE;
 const ctx = canvas.getContext("2d");
+
+canvas.addEventListener("click", event => {
+    const boundingRect = canvas.getBoundingClientRect();
+
+    const scaleX = canvas.width / boundingRect.width;
+    const scaleY = canvas.height / boundingRect.height;
+
+    const canvasLeft = (event.clientX - boundingRect.left) * scaleX;
+    const canvasTop = (event.clientY - boundingRect.top) * scaleY;
+
+    const row = Math.min(Math.floor(canvasTop / TILE_SIZE), canvas.height);
+    const col = Math.min(Math.floor(canvasLeft / TILE_SIZE), canvas.width);
+
+    board.uncover_tile(row, col);
+
+    drawGrid();
+    drawTiles();
+})
 
 const getIndex = (x: number, y: number) => {
     return y * SIZE + x;
@@ -36,10 +54,13 @@ const drawTiles = () => {
     const tilesPtr = board.tiles();
     const tiles = new Uint8Array(memory.buffer, tilesPtr, SIZE ** 2);
 
+    const uncoveredPtr = board.uncovered();
+    const uncovered = new Uint8Array(memory.buffer, uncoveredPtr, SIZE ** 2);
+
     for (let j = 0; j < SIZE; j++) {
         for (let i = 0; i < SIZE; i++) {
             let index = getIndex(i, j);
-            if (tiles[index] === Tile.Mine) {
+            if (uncovered[index] === TileState.Uncovered) {
                 ctx.beginPath();
                 ctx.arc(i * SIZE + SIZE / 2, j * SIZE + SIZE / 2, SIZE / 3, 0, 2 * Math.PI);
                 ctx.fill();
